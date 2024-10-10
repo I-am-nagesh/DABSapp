7ae54cd937835ca718fc2da56d8a8e3c737cbcce
I-am-nagesh <i.am.nageshpandey@gmail.com>
2024-10-10 15:47:07 +0530

    creating admin panel

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,65 +1,110 @@
-import validator from "validator"
-import bycrypt from 'bcrypt'
-import { v2 as cloudinary } from "cloudinary"
-import doctorModel from "../models/doctorModel.js"
-
+import validator from "validator";
+import bycrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
+import jwt from 'jsonwebtoken'
 
 //API for adding doctor
 const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+    const imageFile = req.file;
 
-    try {
-        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
-        const imageFile = req.file
+    //checking for all data to add doctor
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !fees ||
+      !address
+    ) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
 
-        //checking for all data to add doctor
-        if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address){
-            return res.json({success:false, message:"Missing Details"})
+    //validating email format
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "please enter a valid email",
+      });
+    }
+
+    //validating strong password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "please enter a strong password",
+      });
+    }
+
+    //hashing docotr password
+    const salt = await bycrypt.genSalt(10);
+    const hashedPassword = await bycrypt.hash(password, salt);
+
+    // //upload image to cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+
+    res.json({ success: true, message: "Doctor Added" });
+
+    // console.log( { name, email, password, speciality, degree, experience, about, fees, address } , imageFile)
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//API for admin login
+
+const loginAdmin = async (req, res) => {
+    try{
+
+        const {email, password} = req.body
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+
+            const token = jwt.sign(email+password, process.env.JWT_SECRET)
+            res.json({success:true, token})
+
+        }else{
+            res.json({success:false, message:"Invalid credentials"})
         }
 
-        //validating email format
-        if(!validator.isEmail(email)){
-           return res.json({success:false, message:"please enter a valid email"})
-        }
-
-        //validating strong password
-        if(password.length < 8){
-            return res.json({success:false, message:"please enter a strong password"})
-        }
-
-        //hashing docotr password
-        const salt = await bycrypt.genSalt(10)
-        const hashedPassword = await bycrypt.hash(password, salt)
-
-        // //upload image to cloudinary
-        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image"})
-        const imageUrl = imageUpload.secure_url
-
-        const doctorData = {
-            name,
-            email,
-            image:imageUrl,
-            password:hashedPassword,
-            speciality,
-            degree,
-            experience,
-            about,
-            fees,
-            address:JSON.parse(address),
-            date:Date.now()
-        }
-        const newDoctor = new doctorModel(doctorData)
-        await newDoctor.save()
-
-        res.json({success:true, message:"Doctor Added"})
-
-
-        // console.log( { name, email, password, speciality, degree, experience, about, fees, address } , imageFile)
-
-
-    } catch (error) {
-       console.log(error)
-       res.json({success:false, message:error.message})
+    }catch(error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
     }
 }
 
-export {addDoctor}
+export { addDoctor, loginAdmin };
